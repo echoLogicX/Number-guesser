ad4f1cf83afb0f32b738841f41e38fdfe1eb0a3b
echoLogicX <130903179+echoLogicX@users.noreply.github.com>
2024-04-05 15:47:34 +0530

    added guess logic to app.js and added guesses element to index.html

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
     const checkButton = document.getElementById("checkButton");
     const feedback = document.getElementById("feedback");
+    let rand = Math.floor(Math.random()*100)+1;
+    let noOfGuesses = 5;
+    let guesses = document.getElementById("guesses");
+
+    function reset()
+    {
+        feedback.textContent = "";
+        rand = Math.floor(Math.random()*100)+1;
+        noOfGuesses = 5;
+        guesses.textContent = "";
+    }
     checkButton.addEventListener("click", () => {
         const inputField = document.getElementById("guess");
-        let inputVal = inputField.value;
-        let rand = Math.floor(Math.random()*100)+1;
-        if(inputVal<rand)
+        if(noOfGuesses>0)
         {
-            feedback.textContent = "Guess too low";
-        }
-        else if(inputVal>rand)
-        {
-            feedback.textContent = "Guess too high";
+            let inputVal = inputField.value;
+    
+            if(inputVal<rand)
+            {
+                feedback.textContent = "Guess too low";
+                guesses.textContent += (" " + inputVal);
+            }
+            else if(inputVal>rand)
+            {
+                feedback.textContent = "Guess too high";
+                guesses.textContent += (" " + inputVal);
+            }
+            else
+            {
+                feedback.textContent = "Yay! you guessed it right!";
+                reset();
+            }
+            noOfGuesses-=1;
         }
         else
-            feedback.textContent = "Yay! you guessed it right!";
+        {
+            feedback.textContent = "Sorry! you've ran out of guesses"
+        
+        }
     });
 });
